238751197e6223969a6df063c291bb44c2e435de
Harsha <sriharsha.valluru@gmail.com>
2021-07-15 19:36:59 +0530

    done with the backend

--- models/User.js
+++ models/User.js
@@ -2,41 +2,43 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const bcrypt = require("bcrypt");
 
-const UserSchema = ({
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    password: {type: String, required: true },
-    date: { type: Date, default: Date.now }
+//Creating the Schema
+const UserSchema = new Schema({
+  name: { type: String, required: true, unique: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  date: { type: Date, default: Date.now }
 });
 
-//Auth user against database
+//authenticate input against database
 UserSchema.statics.authenticate = function(email, password, callback) {
-    User.findOne({ email: email }).exec(function(err, user) {
-        if (err) {
-            return callback(err);
-        } else if(!user) {
-            var err = new Error("User not found");
-            err.status = 401;
-            return callback(err);
-        }
-        bcrypt.compare(password, user.password, function(err, result) {
-            if (result == true) {
-                return callback(null, user);
-            } else {
-                return callback();
-            }
-        });
+  User.findOne({ email: email }).exec(function(err, user) {
+    if (err) {
+      return callback(err);
+    } else if (!user) {
+      var err = new Error("User not found.");
+      err.status = 401;
+      return callback(err);
+    }
+    bcrypt.compare(password, user.password, function(err, result) {
+      if (result === true) {
+        return callback(null, user);
+      } else {
+        return callback();
+      }
     });
+  });
 };
 
-UserSchema.pre("save", function(next){
-    let user = this;
-    bcrypt.hash(user.password, 10, function(err, hash) {
-        if(err){
-            return next(err);
-        }
-        user.password = hash;
-        next();
-    });
+//hashing a password before saving it to the database
+UserSchema.pre("save", function(next) {
+  let user = this;
+  bcrypt.hash(user.password, 10, function(err, hash) {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  });
 });
 module.exports = User = mongoose.model("user", UserSchema);
