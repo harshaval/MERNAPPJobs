a3ca7b3b55bb9439a997ede13cd8723cffefc4bb
Harsha <sriharsha.valluru@gmail.com>
2021-07-19 22:16:49 +0530

    login and signup commits and configuring react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import NoMatch from "./components/NoMatch";
+import AddJobs from "./components/AddJobs";
+import Footer from "./components/Footer";
+import ContactUs from "./components/ContactUs";
+import Main from "./components/Main";
+import Login from "./components/auth/Login";
+import SignUp from "./components/auth/SignUp";
+import withAuth from "./components/auth/withAuth";
+import checkAuth from "./components/auth/checkAuth";
+import JobPosts from "./components/JobPosts";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/login" component={checkAuth(Login)} />
+          {/* <Route exact path="/login" component={Login} /> */}
+          <Route exact path="/" component={JobPosts} />
+          <Route exact path="/Main" component={Main} />
+          <Route exact path="/ContactUs" component={ContactUs} />
+          <Route exact path="/signup" component={checkAuth(SignUp)} />
+          <Route exact path="/addJobs" component={withAuth(AddJobs)} />
+
+          <Route component={NoMatch} />
+        </Switch>
+        <Footer />
+      </Router>
+    </React.Fragment>
   );
 }
 
